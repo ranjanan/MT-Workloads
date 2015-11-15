b162ff84a16b2e9f3c70d620ad00137f2239128b
Ranjan Anantharaman <ranjan@juliacomputing.com>
2015-11-15 12:31:50 +0000

    Add cpu and gpu benchmarks

---
+++ Graph500/benchmarks/cpu.jl
@@ -0,0 +1,28 @@
+include("../cpu/graph500.jl")
+function run_benchmarks()
+	graph500_cpu(10,10)
+	
+	println("Starting benchmarks ...")
+	for i = 10 : 20
+		println("-----k = $i------")
+		graph500_cpu(i, i)
+	end
+
+	println("Done.")
+end
+
+function run_variance()
+	graph500(10,10)
+	
+	println("Starting benchmarks ...")
+	for i = 10 : 20
+		println("-----k = $i-----")
+		graph500(18, i)
+	end
+
+#	for i = 10 : 20
+#		println("-----k = $i-----")
+#		graph500(i, 15)
+#	end
+	println("Done.")
+end
